6016c25a0116231d2d0d505f67cb195aa48323bc
Rodo <rodobedrossian@gmail.com>
2021-01-19 16:43:56 -0300

    Modificaciones al archivo de python básico

--- python.py
+++ python.py
@@ -5,3 +5,28 @@ numero_uno = 2
 numero_dos = 6
 resultado = numero_uno + numero_dos
 print(resultado)
+
+print("Hola")
+print(55)
+print("Mi nombre es Rodrigo y tengo",24,"años")
+print("Mi nombre es {} y tengo {} años".format("Rodrigo",24))
+print("2 + 2  is {}".format(2*2))
+
+
+# Booleans
+
+a = 45
+b = 10
+print("{} * {} is {}".format(a,b,a*b))
+
+resultado = a*b
+numero = 451
+print(resultado == numero)
+
+
+# Power Operator
+print(2**5) # Es como usar el "elevado al" -> ^5
+
+"""
+Esto se usa para descripciones, en caso de un comentario, usar sólo #
+"""
